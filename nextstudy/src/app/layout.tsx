05f27fd3cf0cc6569454c49a005c243aa3ea4a30
SeongKookKIM <ksk8646@naver.com>
2024-10-25 20:23:59 +0900

    Chore.Recoil 라이브러리 추가 후 Wrapper 컴포넌트 생성

--- nextstudy/src/app/layout.tsx
+++ nextstudy/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { RecoilRoot } from "recoil";
+import RecoilRootWrapper from "./RecoilWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <RecoilRootWrapper>{children}</RecoilRootWrapper>
       </body>
     </html>
   );
