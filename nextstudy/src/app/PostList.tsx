15111152afcb3a2f304bd150cd57efbb37a27a0d
SeongKookKIM <ksk8646@naver.com>
2024-11-14 16:09:03 +0900

    Feat.MySql Get Posts

--- nextstudy/src/app/PostList.tsx
+++ nextstudy/src/app/PostList.tsx
@@ -5,17 +5,43 @@ import { useRecoilValue } from "recoil";
 import style from "./PostList.module.css";
 import { Post } from "@/entities/Post/PostModel";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function PostList() {
   const posts = useRecoilValue(postState);
+  const [dbPosts, setDbPosts] = useState<Post[]>([]);
 
   const router = useRouter();
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("/api/post", {
+          method: "GET",
+        });
+
+        if (!response.ok) {
+          throw new Error("포스트를 가져오는데 실패하였습니다.");
+        }
+
+        const postsData = await response.json();
+
+        console.log(postsData);
+
+        setDbPosts(postsData);
+      } catch (error) {
+        console.error("Data Fetching Failed", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className={style.postList}>
-      {posts.length > 0 ? (
+      {dbPosts.length > 0 ? (
         <ul className={style.postListUl}>
-          {posts.map((post: Post, index: number) => {
+          {dbPosts.map((post: Post, index: number) => {
             return (
               <li
                 key={index}
